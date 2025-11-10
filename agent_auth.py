ebc8d8f482c61dedcc2157f43a19bb1b48bb5d5a
Garvit Joshi <garvitjosh7103@gmail.com>
2025-11-11 03:56:56 +0530

    File has been updated

--- agent_auth.py
+++ agent_auth.py
@@ -1,136 +1,77 @@
-from pymongo import MongoClient
-
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["CourierSystem"]
-
-agents_collection = db["aggents"]
-agencies_collection = db["agencies"]
-
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class LL:
-    def __init__(self):
-        self.head = None
-
-    def append(self, data):
-        new = Node(data)
-        if not self.head:
-            self.head = new
-        else:
-            new.next = self.head
-            self.head = new
-
-    def search(self, val):
-        curr = self.head
-        while curr:
-            if curr.data == val:
-                return curr.data
-            curr = curr.next
-        return None
-    
-    def search_usernameAndPass(self, username = None, password = None):
-        curr = self.head
-        while curr:
-            if curr.data["username"] == username:
-                return curr.data
-            curr = curr.next
-        
-        curr = self.head
-        while curr:
-            if curr.data["password"] == password:
-                return curr.data
-            curr = curr.next
-        
-        return None
-    
-
-
-    def printlist(self):
-        if not self.head:
-            print("no data")
-            return
-        else:
-            curr = self.head
-            while curr:
-                print(curr.data)
-                curr = curr.next    
-
-all_aggencies = agencies_collection.find({}, {"agency": 1, "_id": 0})   
-all_username = agents_collection.find({}, {"username": 1,"password": 1, "_id": 0})
 def sign_up():
-    agencies = LL()
-    for names in all_aggencies:
-        agencies.append(names)
+    username = input("enter the user name: ")
+    file = open("agents.txt", "r")
+    hash = len(username)%10
+    if hash == 0: hash = 10
+    for i in range(hash):
+        line = file.readline()
+
+    if username in line:
+        print("Username is already taken")
+        return
+
+    if " " in username:
+        print("Username cant have spaces")
+        return
+ 
+    file.close()
+
+    agency_name =input("please verify your company name: ")
+    file=open("agency.txt","r")
+    hash = len(agency_name)%10
+    if hash == 0: hash = 10
+    for i in range(hash):
+        line = file.readline()
     
-    usernames = LL()
-    for names in all_username:
-        usernames.append(names)
+    file.close()
+    if agency_name not in line:
+        print("Agency does not exist")
+        return
+
+    password = input("Enter the password: ")
+    upper = False
+    lower = False
+    number = False
+    for i in password:
+        if len(password) < 8:
+            print("More than 8 character")
+            return
+        if (ord(i)>=ord('A') and ord(i)<=ord('Z')):
+            upper = True
+        if (ord(i)>=ord('a') and ord(i)<=ord('z')):
+            lower = True
+        if i.isnumeric():
+            number = True
     
-    agent_agency = input("Enter the agency name: ")
-    agent_id = int(input("Enter the Id: "))
-    agent_username = input("enter the user name: ")
-    agent_password = input("Enter the password: ")
+    if upper == False or lower == False or number == False:
+        print("Invalid password")
+        return
 
-    if(agencies.search({'agency':agent_agency}) == None) :
-        print("Your agency is not registed")
-        return False
+    with open("agents.txt", "r") as file:
+        line = file.readlines()
+        line[hash-2] = line[hash-2] + " " + username+" - "+password+" - "+agency_name
 
-    if(" " in agent_username): 
-        print("username can't have spaces")
-        return False
-    if(usernames.search_usernameAndPass(agent_username) != None):
-        print("username already taken ")
-        return  False
 
-    data = {
-        "agency": agent_agency,
-        "agent_id": agent_id,
-        "username": agent_username,
-        "password": agent_password
-    }
-
-    agents_collection.insert_one(data)
-    return True
-
+    with open("agents.txt", "w") as f:
+        f.writelines(line)
+    
+    print("agent details have been saved successfully")
 
 def sign_in():
-    agencies = LL()
-    for names in all_aggencies:
-        agencies.append(names)
-    
-    usernames = LL()
-    for names in all_username:
-        usernames.append(names)
-    
     username = input("Enter the username: ")
     password = input("Enter the password: ")
+    hash = len(username) %10
+    if(hash == 0): hash = 10
 
-    if(usernames.search_usernameAndPass(username) == None):
-        print("Invalid user name")
-        return False
-    
-    if(usernames.search_usernameAndPass(username, password) == None):
-        print("Wrong password")
-        return False
-    
-    print(f"Welcome {username}")
-    return True
-    
+    with open("agents.txt", "r") as f:
+        for i in range(0,hash):
+            line = f.readline()
+        
+        new = username+" - "+password
+        if new not in line:
+            print("Wrong username or password")
+        else:
+            print(f"Welcome {username}")
 
 
-def main():
-    print("Welcome Agent")
-    print("1. Create new Account\n2.Login")
-    choice = int(input("Enter the choice: "))
-    flag  = False
-    while flag != True: 
-        if choice == 1:
-            flag = sign_up()
-        else: 
-            flag = sign_in()
-    return
+sign_up()
